badae03290d43c4db4a2975466220701938717d9
Laura Beth Maddox <laurabethmaddox@gmail.com>
2022-10-11 14:41:58 -0500

    Clients showing

--- src/components/RealEstate.js
+++ src/components/RealEstate.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const RealEstate = () => {
+    const [clients, updateClients] = useState([])
+
+    useEffect(
+        () => {
+            fetch("http://localhost:8088/clients")
+                .then(res => res.json())
+                .then((clientArray) => {
+                    updateClients(clientArray)
+                })
+        }, 
+        []
+    )
 
     return (
-        <h1>Real Estate Services</h1>
+        <>
+            <h1>Real Estate Services</h1>
+
+            {
+                clients.map(
+                    (clientObj) => {
+                        return <h2>{clientObj.name}</h2>
+                    }
+                )
+            }
+        </>
     )
 }
